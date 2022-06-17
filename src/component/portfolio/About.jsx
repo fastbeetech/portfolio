b218f0653995bd47d083d69185b42459ba128d99
fastbeetech <hezekiahfasanya@gmail.com>
2022-06-18 00:05:20 +0100

    about edit

--- src/component/portfolio/About.jsx
+++ src/component/portfolio/About.jsx
@@ -11,7 +11,7 @@ const About = (props) => {
       exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}
       className="w-full bg-zinc-100"
     >
-      <Container className="container my-10 sm:px-5 lg:px-14 py-3">
+      <Container className="lg:h-[60vh] sm:h-auto container my-10 sm:px-5 lg:px-14 py-3">
         <div className="my-3 space-y-5">
           <h2 className="text-2xl font-bold">About Me</h2>
           <p className="">
