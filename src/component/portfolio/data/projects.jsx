89bd624b99c7738ca1e126b66eb8c19d93bc16e5
fastbeetech <hezekiahfasanya@gmail.com>
2022-06-17 10:45:15 +0100

    adjust

--- src/component/portfolio/data/projects.jsx
+++ src/component/portfolio/data/projects.jsx
@@ -35,7 +35,7 @@ const Projects = [
     {
         id: 5,
         title: 'Trade By Barter',
-        description: 'A movie app created with livewire, tailwindCss and Alpine Js to understand how livewire works and to master blade concept of laravel',
+        description: 'A movie app created with livewire, tailwindCss and Alpine Js to understand how livewire works in Laravel',
         image: 'http://res.cloudinary.com/fastbeetech/image/upload/v1655455013/eucpzpansg1gusacakez.png',
         link: 'https://movies-livewire.herokuapp.com/',
         github: 'https://github.com/fastbeetech/livewire-movieapp'
